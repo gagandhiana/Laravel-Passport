91df3545200742a0454446b7c058ef9f37aece75
gagandhiana <kaurgagandeep7009gmail.com>
2022-12-29 17:12:36 +0530

    added

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// https://github.com/anil-sidhu/laravel-passport-poc  (Refrence)
+
+
+Route::post('register', 'App\Http\Controllers\API\UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', 'App\Http\Controllers\API\UserController@details');
+});
+
+
+Route::post('login', 'App\Http\Controllers\API\UserController@login'); 
