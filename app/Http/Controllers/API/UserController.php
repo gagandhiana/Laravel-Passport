df9cb7a059e092c4c7e630248bb2fd377036a248
Gagandeep Kaur <106575960+gagandhiana@users.noreply.github.com>
2023-01-09 12:29:17 +0530

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -28,20 +28,20 @@ class UserController extends Controller
         $user = User::create($input); 
         $success['token'] =  $user->createToken('MyApp')-> accessToken; 
         $success['name'] =  $user->name;
-        return response()->json(['success'=>$success], $this-> successStatus); 
+        return response()->json(['success'=>$success]); 
     }
 
     public function details() 
     { 
         $user = Auth::user(); 
-        return response()->json(['success' => $user], $this-> successStatus); 
+        return response()->json(['success' => $user]); 
     }
     
     public function login(){ 
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
             $user = Auth::user(); 
             $success['token'] =  $user->createToken('MyApp')-> accessToken; 
-            return response()->json(['success' => $success], $this-> successStatus); 
+            return response()->json(['success' => $success]); 
         } 
         else{ 
             return response()->json(['error'=>'Unauthorised'], 401); 
